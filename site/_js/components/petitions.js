16aa61998f07c4ae0b682da28336f8ce69821a1e
Aki Rose Braun <hi@akiro.se>
2016-01-08 16:53:11 -0800

    Prose-based petitions first add 🎉

---
+++ site/_js/components/petitions.js
@@ -0,0 +1,117 @@
+window.components = window.components || {};
+window.components.petitions = function (doc, win) {
+  "use strict";
+
+  function numberCommafier(number) {
+    /**
+     * Returns a string representing a number with commas
+     * @param {int} number - the number to transform
+     * */
+
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  function progressBar(targetValue, targetGoal) {
+    /**
+     * Animates the value of a progress bar
+     * @param {int} targetValue - the target value attribute of the progress bar
+     * @param {int} targetGoal - the target max attribute of the progress bar
+     * */
+
+    var
+      guardedTargetVal = targetValue.isNaN ? 0 : targetValue,
+      animate,
+      value = 0,
+      progressbar = doc.getElementById('signatures-progress-bar'),
+      max = parseInt(targetGoal),
+      step = (guardedTargetVal / 1500) * 30; // 1500 ms total, 30ms minimum interval
+
+    function loading() {
+      value += step;
+      value = Math.ceil(value);
+      if (value >= guardedTargetVal || value >= max) {
+        value = guardedTargetVal;
+        clearInterval(animate);
+      }
+
+      var
+        commafiedNumber = numberCommafier(value);
+
+      progressbar.setAttribute('max', max);
+      progressbar.setAttribute('value', value);
+      progressbar.setAttribute('title', commafiedNumber + ' signatures');
+      doc.getElementById('total-sigs').innerText = commafiedNumber;
+      doc.getElementById('sigs-to-go').innerText = numberCommafier(targetGoal - value) + ' needed to reach ' + numberCommafier(targetGoal);
+      progressbar.innerHTML = commafiedNumber + ' signatures';
+    }
+
+    animate = setInterval(function () {
+      loading();
+    }, 30);
+  }
+
+  function handleError() {
+    /**
+     * Sets values for the progress bar even if there's a server or XHR error
+     * */
+    progressBar(972, 1600);
+  }
+
+  function readMoreButtons() {
+    /**
+     * Makes content visible!
+     * */
+    var
+      buttons = doc.getElementsByClassName('expand-text'),
+      text = doc.getElementsByClassName('expanded-text'),
+      i = buttons.length;
+
+    function expandText(e) {
+      /**
+       * Changes max-height on a given post.
+       * */
+      var
+        targetOrder = e.target.dataset.order;
+
+      text[targetOrder].style.maxHeight = 10000 + 'px';
+      buttons[targetOrder].setAttribute('class', 'hidden');
+    }
+
+    while (i--) {
+      buttons[i].dataset.order = i;
+      text[i].dataset.order = i;
+      buttons[i].addEventListener('click', expandText);
+    }
+  }
+
+  function requestAPIInfo() {
+    /**
+     * Builds and sends request to API server
+     * */
+    var
+      title = doc.getElementById('petition-title').innerText,
+      anRequest = new XMLHttpRequest();
+
+    anRequest.open('GET', doc.forms[0].dataset.host + '/petition?title=' + title, true);
+    anRequest.addEventListener('load', function () {
+      if (anRequest.status >= 200 && anRequest.status < 400) {
+        var
+          apiData = JSON.parse(anRequest.responseText);
+
+        progressBar(apiData.signatures, apiData.goal);
+        doc.forms[0].setAttribute('action', apiData.action);
+      } else {
+        handleError();
+      }
+    });
+    anRequest.addEventListener('error', handleError);
+    anRequest.send();
+  }
+
+  function init() {
+    requestAPIInfo();
+    readMoreButtons();
+  }
+
+  init();
+};
--- site/_js/main.js
+++ site/_js/main.js
@@ -1,4 +1,21 @@
 (function (doc, win) {
   "use strict";
 
+  win.components = win.components || {};
+
+  var
+    i = 0,
+    components = doc.getElementsByTagName('body')[0].dataset.components;
+
+  if (components !== undefined) {
+    components = components.split(' ');
+    i = components.length;
+  }
+
+  while (i--) {
+    if (components[i] !== '' && win.components[components[i]] !== undefined) {
+      win.components[components[i]](doc, win);
+    }
+  }
+
 })(document, window);
