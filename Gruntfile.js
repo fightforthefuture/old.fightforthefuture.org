947a79a71538f94d06ee8099a2bbf2f0f7aabeb0
Aki Rose Braun <hi@akiro.se>
2016-01-21 20:43:48 -0800

    Adds window.components

--- Gruntfile.js
+++ Gruntfile.js
@@ -173,7 +173,7 @@ module.exports = function (grunt) {
         files: [
           {
             src: [
-              '<%= site.app %>/_js/controllers/**/*.js',
+              '<%= site.app %>/_js/components/**/*.js',
               '<%= site.app %>/_js/models/**/*.js',
               '<%= site.app %>/_js/views/**/*.js',
               '<%= site.app %>/_js/main.js'
