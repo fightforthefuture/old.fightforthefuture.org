c08f31da9c79cfc5aaeb654e54265acd4fecf021
Aki Rose Braun <hi@akiro.se>
2016-02-03 18:14:34 -0800

    Prepares data to be sent off to the fftf Action Network API server

--- site/_js/components/petitions.js
+++ site/_js/components/petitions.js
@@ -3,9 +3,12 @@ window.components.petitions = function (doc, win) {
   "use strict";
 
   var
+    apiHost = doc.forms[0].dataset.host,
     body = doc.getElementsByTagName('body')[0],
-    countrySelect = doc.getElementById('country'),
-    countryLabel = doc.querySelector('[for="country"]');
+    objectIdentifier = false,
+    countryInput = doc.getElementById('hidden-country'),
+    countrySelect = doc.getElementById('select-country'),
+    countryLabel = doc.querySelector('[for="select-country"]');
 
   function numberCommafier(number) {
     /**
@@ -55,7 +58,7 @@ window.components.petitions = function (doc, win) {
     }, 30);
   }
 
-  function handleError() {
+  function handleProgressBarError() {
     /**
      * Sets values for the progress bar even if there's a server or XHR error
      * */
@@ -74,6 +77,7 @@ window.components.petitions = function (doc, win) {
     function expandText(e) {
       /**
        * Changes max-height on a given post.
+       * @param {event} e - Event passed from listener
        * */
       var
         targetOrder = e.target.dataset.order;
@@ -97,23 +101,24 @@ window.components.petitions = function (doc, win) {
       title = doc.getElementById('petition-title').innerText,
       anRequest = new XMLHttpRequest();
 
-    anRequest.open('GET', doc.forms[0].dataset.host + '/petition?title=' + title, true);
+    anRequest.open('GET', apiHost + '/petition?title=' + title, true);
     anRequest.addEventListener('load', function () {
       if (anRequest.status >= 200 && anRequest.status < 400) {
         var
           apiData = JSON.parse(anRequest.responseText);
 
+        objectIdentifier = apiData.objectID;
         progressBar(apiData.signatures, apiData.goal);
         doc.forms[0].setAttribute('action', apiData.action);
       } else {
-        handleError();
+        handleProgressBarError();
       }
     });
-    anRequest.addEventListener('error', handleError);
+    anRequest.addEventListener('error', handleProgressBarError);
     anRequest.send();
   }
 
-  function updateZIPPlaceholder(e) {
+  function updateZIPPlaceholder(event) {
     /**
      * Updates placeholder on ZIP/Post Code field to be appropriate for country
      * selected
@@ -129,15 +134,90 @@ window.components.petitions = function (doc, win) {
     }
   }
 
-  function toggleCountryField(e) {
+  function toggleCountryField(event) {
     /**
      * Hides the label and shows the select when someone changes their signature
      * country.
      * @param {event} e - Doesn't do anything atm.
      * */
 
-    countryLabel.classList.toggle('hidden');
-    countrySelect.classList.toggle('visible');
+    if (countryInput.getAttribute('name')) {
+      countryInput.removeAttribute('name');
+      countrySelect.setAttribute('name', 'signature[country]');
+      countrySelect.classList.add('hidden');
+      countryLabel.classList.remove('hidden');
+    } else {
+      countrySelect.removeAttribute('name');
+      countryInput.setAttribute('name', 'signature[country]');
+      countrySelect.classList.remove('hidden');
+      countryLabel.classList.add('hidden');
+    }
+  }
+
+  function handleSigningError(error) {
+    console.log(error);
+  }
+
+  function submitForm(event) {
+    /**
+     * Submits the form to ActionNetwork. If the script doesn’t, by now, know
+     * the action_network identifier, default isn’t prevented on the event and
+     * form submission proceeds as normal.
+     * @param {event} event - Form submission event
+     * */
+
+    if (objectIdentifier) {
+      event.preventDefault();
+    }
+
+    var
+      formValues = doc.forms[0],
+      signatureSubmission = new XMLHttpRequest(),
+      petitionSignaturePayload = {
+        identifier: objectIdentifier.split(':')[1],
+        referrer: win.location.toString(),
+        website: win.location.origin,
+        ZIP: doc.getElementById('form-zip_code').value,
+        country: doc.getElementById('select-country').value,
+        email: doc.getElementById('form-email').value,
+        tags: JSON.parse(doc.querySelector('[name="subscription[tag_list]"]').value)
+      };
+
+    if (doc.getElementById('opt-in').checked !== false) {
+      petitionSignaturePayload.optin = 'subscribed';
+    }
+
+    if (doc.getElementById('form-street_address')) {
+      petitionSignaturePayload.address = [doc.getElementById('form-street_address').value];
+    }
+
+    if (doc.getElementById('form-first_name')) {
+      petitionSignaturePayload.name = doc.getElementById('form-first_name').value;
+    }
+
+    if (doc.getElementById('form-city')) {
+      petitionSignaturePayload.city = doc.getElementById('form-city').value;
+    }
+
+    if (doc.getElementById('form-comments')) {
+      petitionSignaturePayload.comments = doc.getElementById('form-comments').value;
+    }
+
+    signatureSubmission.open('POST', doc.forms[0].dataset.host + '/signature', true);
+    signatureSubmission.setRequestHeader('Content-Type', 'application/json');
+
+    signatureSubmission.addEventListener('load', function () {
+      if (signatureSubmission.status >= 200 && signatureSubmission.status < 400) {
+        new win.models.modals.ShareDaisyModalController({
+          modal_content: '<h2>Thanks for signing</h2>\n<p>Now, share this page to spread the word.</p>\n<p><small>…or, <a href="https://donate.fightforthefuture.org/?amount=5&frequency=just-once">chip in $5</a> to help us spread the message.</small></p>'
+        });
+      } else {
+        handleSigningError();
+      }
+    });
+
+    signatureSubmission.addEventListener('error', handleSigningError);
+    signatureSubmission.send(JSON.stringify(petitionSignaturePayload));
   }
 
   function addEventListeners() {
@@ -146,12 +226,19 @@ window.components.petitions = function (doc, win) {
      * */
     countryLabel.addEventListener('click', toggleCountryField);
     countrySelect.addEventListener('change', updateZIPPlaceholder);
+    doc.forms[0].addEventListener('submit', submitForm);
   }
 
   function init() {
     requestAPIInfo();
     readMoreButtons();
     addEventListeners();
+
+    /*
+     new win.controllers.modals.ShareDaisyModalController({
+     modal_content: '<h2>Thanks for signing</h2>\n<p>Now, share this page to spread the word.</p>\n<p><small>…or, <a href="https://donate.fightforthefuture.org/?amount=5&frequency=just-once&utm">chip in $5</a> to help us spread the message.</small></p>'
+     });
+     */
   }
 
   init();
