e261c117a33f000bd15bf9d60bf2364d823e8b31
Aki Rose Braun <hi@akiro.se>
2016-04-07 00:07:57 -0700

    Adds incremental build support to jekyll for faster local builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,7 @@ module.exports = function (grunt) {
       },
       server: {
         options: {
-          config: '_config.yml'
+          incremental: true
         }
       },
       check: {
